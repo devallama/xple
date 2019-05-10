496e4f3e8f6ca43a348eb998cae79c9b2811fc86
nickallama <me@nicholasneale.com>
2019-05-10 09:58:34 +0100

    Projects page

---
+++ src/js/pages/Projects.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+class Projects extends React.Component {
+    render() {
+        return (
+            <div className="row column medium-3">
+                <h1>Projects</h1>
+            </div>
+        );
+    }
+}
+
+export default Projects;
