480eddb80d5e8c486a17146819c2ce8b979faa89
nickallama <me@nicholasneale.com>
2019-05-10 09:57:16 +0100

    Action types

---
+++ src/js/actions/types.js
@@ -0,0 +1,13 @@
+/* User Action types */
+export const USER_IS_LOGGED_IN = 'USER_IS_LOGGED_IN';
+export const USER_FETCH = 'USER_FETCH';
+
+/* Firebase Action types */
+
+export const FIREBASE_FETCH_INSTANCE = 'FIREBASE_FETCH_INSTANCE';
+
+/* Auth Action types */
+
+export const AUTH_LOGIN = 'AUTH_LOGIN';
+export const AUTH_REGISTER = 'AUTH_REGISTER';
+export const AUTH_LOGOUT = 'AUTH_LOGOUT';
