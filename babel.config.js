fbff576d543c340ba774ed83ff9be2d36727c4aa
nickallama <me@nicholasneale.com>
2019-05-10 09:52:31 +0100

    Babel config

---
+++ babel.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    presets: ['@babel/preset-env', '@babel/preset-react'],
+    plugins: ['@babel/plugin-proposal-class-properties'],
+    env: {
+        test: {
+            plugins: [
+                ["babel-plugin-webpack-alias"]
+            ]
+        }
+    }
+}
