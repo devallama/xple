e1540560c45af5eec900fd17390e07b13ef3d2ea
nickallama <me@nicholasneale.com>
2019-05-12 20:22:51 +0100

    import new reducers

--- src/js/reducers/index.js
+++ src/js/reducers/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
 
+import appDBReducer from './app-db-reducer';
 import authReducer from './auth-reducer';
 import firebaseReducer from './firebase-reducer';
+import learningAppsReducer from './learning-apps-reducer';
 import locationReducer from './location-reducer';
 import milestoneReducer from './milestone-reducer';
 import projectReducer from './project-reducer';
@@ -9,8 +11,10 @@ import referenceReducer from './reference-reducer';
 import userReducer from './user-reducer';
 
 export default combineReducers({
+    appDB: appDBReducer,
     auth: authReducer,
     firebase: firebaseReducer,
+    learningApp: learningAppsReducer,
     location: locationReducer,
     milestone: milestoneReducer,
     project: projectReducer,
