bc6b37fe51dff3b60f8ff84a4a3c1c27026e6d38
nickallama <me@nicholasneale.com>
2019-05-12 21:57:52 +0100

    Project card links

--- src/js/pages/ProjectHome.jsx
+++ src/js/pages/ProjectHome.jsx
@@ -1,37 +1,51 @@
 import React from 'react';
-
-import Frame from 'react-frame-component';
+import { Link } from 'react-router-dom';
 
 class ProjectHome extends React.Component {
     render() {
-        console.log(this);
+        const projectId = this.props.computedMatch.params.projectId;
+
         return (
             <div>
-                Project home
+                <div className="row">
+                    <div className="column medium-6">
+                        <h1 className="icon-title icon-title--project">Project Home</h1>
+                    </div>
+                </div>
+                <div className="row column main-content overflow--hidden">
+                    <div className="card__grid">
+                        <div className="card__item">
+                            <Link to={`/project/${projectId}/learning-apps`} className="link link--black card__link" >
+                                <h2 className="icon-title icon-title--learning-apps card__title">Learning Apps</h2>
+                                <p className="size-large">Use learning tools to help you learn.</p>
+                                <img className="card__icon" src="/assets/icons/arrow-cta.svg" alt="Go" />
+                            </Link>
+                        </div>
 
-                <Test>
-                    <Frame>
-                        <Test2 name={"ted"} />
-                    </Frame>
-                </Test>
-            </div>
-        );
-    }
-}
+                        <div className="card__item">
+                            <Link to={`/project/${projectId}/references`} className="link link--black card__link" >
+                                <h2 className="icon-title icon-title--references card__title">References</h2>
+                                <p className="size-large">Manage your references for your project.</p>
+                                <img className="card__icon" src="/assets/icons/arrow-cta.svg" alt="Go" />
+                            </Link>
+                        </div>
 
-class Test extends React.Component {
-    render() {
-        console.log(this.props.children);
-
-        return this.props.children;
-    }
-}
-
-class Test2 extends React.Component {
-    render() {
-        return (
-            <div>
-                React component - {this.props.name}
+                        <div className="card__item">
+                            <Link to={`/project/${projectId}/timeline`} className="link link--black card__link" >
+                                <h2 className="icon-title icon-title--timeline card__title">Timeline</h2>
+                                <p className="size-large">See the timeline of your project milestones.</p>
+                                <img className="card__icon" src="/assets/icons/arrow-cta.svg" alt="Go" />
+                            </Link>
+                        </div>
+                        <div className="card__item">
+                            <Link to={`/project/${projectId}/milestones`} className="link link--black card__link" >
+                                <h2 className="icon-title icon-title--milestones card__title">Milestones</h2>
+                                <p className="size-large">Manage your tasks, milestones and deadlines.</p>
+                                <img className="card__icon" src="/assets/icons/arrow-cta.svg" alt="Go" />
+                            </Link>
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
