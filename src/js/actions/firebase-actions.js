61687249876a7dd845c6179ecef330efa44d787e
nickallama <me@nicholasneale.com>
2019-05-12 09:49:33 +0100

    Added persistence to firestore data for offline mode

--- src/js/actions/firebase-actions.js
+++ src/js/actions/firebase-actions.js
@@ -5,6 +5,19 @@ import firebaseConfig from '../firebase.confid';
 
 firebase.initializeApp(firebaseConfig);
 
+firebase.firestore().enablePersistence()
+    .catch(function (err) {
+        if (err.code == 'failed-precondition') {
+            // Multiple tabs open, persistence can only be enabled
+            // in one tab at a a time.
+            // ...
+        } else if (err.code == 'unimplemented') {
+            // The current browser does not support all of the
+            // features required to enable persistence
+            // ...
+        }
+    });
+
 export const firebaseFetchInstance = () => dispatch => {
     dispatch({
         type: FIREBASE_FETCH_INSTANCE,
