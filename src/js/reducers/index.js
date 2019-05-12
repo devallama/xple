535b86b5aa9151c83f9d8d4628c669c5cf5a2317
nickallama <me@nicholasneale.com>
2019-05-12 09:55:53 +0100

    Added reducers

--- src/js/reducers/index.js
+++ src/js/reducers/index.js
@@ -2,10 +2,18 @@ import { combineReducers } from 'redux';
 
 import authReducer from './auth-reducer';
 import firebaseReducer from './firebase-reducer';
+import locationReducer from './location-reducer';
+import milestoneReducer from './milestone-reducer';
+import projectReducer from './project-reducer';
+import referenceReducer from './reference-reducer';
 import userReducer from './user-reducer';
 
 export default combineReducers({
     auth: authReducer,
     firebase: firebaseReducer,
+    location: locationReducer,
+    milestone: milestoneReducer,
+    project: projectReducer,
+    reference: referenceReducer,
     user: userReducer
 });
