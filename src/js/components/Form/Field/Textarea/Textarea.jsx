3f77714cb0bc992ebc468259113ef9623d138e01
nickallama <me@nicholasneale.com>
2019-05-12 09:51:59 +0100

    Text area element

---
+++ src/js/components/Form/Field/Textarea/Textarea.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class Textarea extends React.Component {
+    render() {
+        const {
+            name,
+            type,
+            ...rest
+        } = this.props;
+
+        return (
+            <textarea id={`textarea_${name}`} name={name} className={`form__textarea`} {...rest} />
+        );
+    }
+}
+
+Textarea.propTypes = {
+    name: PropTypes.string,
+    value: PropTypes.string,
+    onChange: PropTypes.func,
+    onFocus: PropTypes.func,
+    onBlur: PropTypes.func
+};
+
+export default Textarea;
