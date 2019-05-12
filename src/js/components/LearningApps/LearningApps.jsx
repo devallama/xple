5135de2ba9a8c692095c0b2833d8cce2872e61a6
nickallama <me@nicholasneale.com>
2019-05-12 21:56:48 +0100

    Apps cards

--- src/js/components/LearningApps/LearningApps.jsx
+++ src/js/components/LearningApps/LearningApps.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { learningAppsFetch } from 'Actions/learning-app-actions';
@@ -18,7 +19,25 @@ class LearningApps extends React.Component {
 
             return <App app={app} />
         } else {
-            return null;
+            let cards = <p className="size-large">No learning apps available.</p>;
+
+            if (this.props.apps.length > 0) {
+                cards = this.props.apps.map(app => (
+                    <div className="card__item">
+                        <Link to={`learning-apps/${app.path}`} className="link link--black card__link" >
+                            <h2 className="card__title">{app.name}</h2>
+                            <p className="size-large">{app.description}</p>
+                            <img className="card__icon" src="/assets/icons/arrow-cta.svg" alt="Go" />
+                        </Link>
+                    </div>
+                ));
+            }
+
+            return (
+                <div className="card__grid">
+                    {cards}
+                </div>
+            );
         }
 
     }
