ab2f37588c7397e59a73df37e45e7805862ab797
nickallama <me@nicholasneale.com>
2019-05-12 20:20:37 +0100

    app db action to get app collection

---
+++ src/js/actions/app-db-actions.js
@@ -0,0 +1,12 @@
+import { APP_DB_FETCH } from './types';
+
+export const appDBFetch = app => (dispatch, getState) => {
+    const db = getState().firebase.instance.firestore();
+
+    const collection = db.collection('apps-storage').doc(app).collection('data');
+
+    dispatch({
+        type: APP_DB_FETCH,
+        data: collection
+    });
+}
