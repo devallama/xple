0fc161781f0d19bccd2c426c4467ecae8594744e
nickallama <me@nicholasneale.com>
2019-05-12 20:20:56 +0100

    Removed comments

--- src/js/actions/firebase-actions.js
+++ src/js/actions/firebase-actions.js
@@ -7,15 +7,7 @@ firebase.initializeApp(firebaseConfig);
 
 firebase.firestore().enablePersistence()
     .catch(function (err) {
-        if (err.code == 'failed-precondition') {
-            // Multiple tabs open, persistence can only be enabled
-            // in one tab at a a time.
-            // ...
-        } else if (err.code == 'unimplemented') {
-            // The current browser does not support all of the
-            // features required to enable persistence
-            // ...
-        }
+        if (err.code == 'failed-precondition') { } else if (err.code == 'unimplemented') { }
     });
 
 export const firebaseFetchInstance = () => dispatch => {
