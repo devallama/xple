b4738f4bf00a57445d93e6346167b7f7e65583fa
nickallama <me@nicholasneale.com>
2019-05-21 08:47:36 +0100

    Create styled component flash card app

--- learning-apps/flashcards/app.jsx
+++ learning-apps/flashcards/app.jsx
@@ -1,23 +1,85 @@
 import React from 'react';
 
+import styled from 'styled-components';
+
+import ViewHome from './views/home';
+import ViewCreate from './views/create';
+import ViewPlay from './views/play';
+
+const Views = {
+    home: ViewHome,
+    create: ViewCreate,
+    play: ViewPlay
+};
+
+const Wrapper = styled.div`
+    background-color: #EAEEF6;
+    padding: 1rem;
+    box-sizing: border-box;
+`;
+
+const Header = styled.div`
+    display: flex;
+    align-items: center;
+    margin-bottom: 1.5rem;
+`;
+
+const Heading = styled.h1`
+    display: inline-block;
+    margin: 0.5rem 0 0;
+`;
+
+const HomeLink = styled.button`
+    display: inline-block;
+    margin-left: 2rem;
+    font-size: 1.5rem;
+    margin-top: 0.5rem;
+`;
+
 class FlashcardsApp extends React.Component {
     constructor(props) {
         super(props);
 
-        this.props.db.doc("LA").set({
-            name: "Los Angeles",
-            state: "CA",
-            country: "USA"
+        this.state = {
+            currentView: 'home',
+            flashstacks: []
+        }
+
+        this.getUserFlashStacks();
+
+        // this.props.db;
+    }
+
+    getUserFlashStacks = () => {
+        console.log(this.props.db);
+        this.props.db.collection("flashstacks").where("uid", "==", this.props.user.uid)
+            .onSnapshot(querySnapshot => {
+                const data = querySnapshot.docs.map(doc => { return { id: doc.id, ...doc.data() } });
+
+                this.setState({
+                    flashstacks: data
+                })
+            });
+    }
+
+    changeView = view => {
+        this.setState({
+            currentView: view
         });
     }
 
     render() {
+        const View = Views[this.state.currentView] || Views.home;
         return (
-            <div className="flashcards-container">
-                Flashcards
-                <div className="btn">Button</div>
-                test
-            </div>
+            <Wrapper className="flashcards-container">
+                <div className="row column">
+                    <Header>
+                        <Heading>Flashcards</Heading>
+                        <HomeLink className="link link--purple" onClick={() => this.changeView('home')}>home</HomeLink>
+                    </Header>
+                </div>
+                <View {...this.props} changeView={this.changeView} />
+            </Wrapper>
         );
     }
 }
---
+++ learning-apps/flashcards/views/create.jsx
@@ -0,0 +1,93 @@
+import React from 'react';
+
+class ViewCreate extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            flashstackName: '',
+            flashstack: [{
+                id: 0,
+                front: '',
+                back: ''
+            }]
+        };
+    }
+
+    saveFlashStack = () => {
+        const data = {
+            name: this.state.flashstackName,
+            flashstack: this.state.flashstack,
+            uid: this.props.user.uid
+        }
+        this.props.db.collection("flashstacks").set(data)
+            .then(() => {
+                console.log("success");
+            })
+            .catch(err => {
+                console.log(err);
+            });
+    }
+
+    addToFlashstack = () => {
+        const nextId = Math.max(...Object(this.state.flashstack).keys()) + 1;
+        const flashcard = {
+            id: nextId,
+            front: '',
+            back: ''
+        };
+
+        this.setState({
+            flashstack: [...this.state.flashstack, flashcard]
+        });
+    }
+
+    handleInputChange = (event, id) => {
+        const { name, value } = event.target;
+
+        let flashstack = this.state.flashstack;
+        flashstack[id][name] = value;
+
+        this.setState({
+            flashstack: flashstack
+        });
+    }
+
+    handleInputChangeSingle = event => {
+        const { name, value } = event.target;
+
+        this.setState({
+            [name]: value
+        });
+    }
+
+    render() {
+        console.log(this.state.flashstack);
+        const fields = this.state.flashstack.map(flashcard => (
+            <fieldset key={flashcard.id}>
+                <input type="text" name="front" value={flashcard.front} onChange={event => this.handleInputChange(event, flashcard.id)} />
+                <input type="text" name="back" value={flashcard.back} onChange={event => this.handleInputChange(event, flashcard.id)} />
+            </fieldset>
+        ));
+
+        console.log(fields);
+
+        return (
+            <div>
+                Create flash cards
+
+                <form onSubmit={this.saveFlashStack}>
+                    <div>
+                        Flashstack name: <input type="type" name="flashstackName" value={this.state.flashstackName} onChange={this.handleInputChangeSingle} />
+                    </div>
+                    {fields}
+                    <div>
+                        <button type="submit">Create</button>
+                    </div>
+                </form>
+            </div>
+        );
+    }
+}
+
+export default ViewCreate;
